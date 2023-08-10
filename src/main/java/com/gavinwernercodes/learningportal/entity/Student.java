15560ac1b8656439cefd2d16a6f3e950e02f0a6e
Gavin Werner <gavin.werner.2@gmail.com>
2023-08-10 09:56:06 -0400

    Initial commit

---
+++ src/main/java/com/gavinwernercodes/learningportal/entity/Student.java
@@ -0,0 +1,19 @@
+package com.gavinwernercodes.learningportal.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+}
