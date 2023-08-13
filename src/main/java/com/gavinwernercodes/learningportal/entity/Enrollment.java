749c9dd963d8a525daebd487248430fa15f78086
Gavin Werner <gavin.werner.2@gmail.com>
2023-08-12 23:23:55 -0400

    Added a new entity file for the Enrollment entity and set its ID field.

---
+++ src/main/java/com/gavinwernercodes/learningportal/entity/Enrollment.java
@@ -0,0 +1,12 @@
+package com.gavinwernercodes.learningportal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "enrollments")
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
