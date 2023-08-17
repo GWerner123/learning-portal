23d2aca3baf71d121609eceedf7f9cdb6f82bc34
Gavin Werner <gavin.werner.2@gmail.com>
2023-08-17 00:16:41 -0400

    Added post methods to the registration controller with appropriate endpoint mappings.

--- src/main/java/com/gavinwernercodes/learningportal/controller/RegistrationController.java
+++ src/main/java/com/gavinwernercodes/learningportal/controller/RegistrationController.java
@@ -1,5 +1,10 @@
 package com.gavinwernercodes.learningportal.controller;
 
+import com.gavinwernercodes.learningportal.entity.Instructor;
+import com.gavinwernercodes.learningportal.entity.Student;
+import com.gavinwernercodes.learningportal.service.RegistrationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/registration")
 public class RegistrationController {
 
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping("/instructor")
+    public void createInstructor(@RequestBody Instructor instructor) {
+        registrationService.createInstructor(instructor);
+    }
+
+    @PostMapping("/student")
+    public void createStudent(@RequestBody Student student) {
+        registrationService.createStudent(student);
+    }
 }
