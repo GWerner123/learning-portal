8c85acacfc67915cfe9b48f1967ca5d6b5ea9d37
Gavin Werner <gavin.werner.2@gmail.com>
2023-08-21 06:24:30 -0400

    Added a course_title field to the course entity and created public getter and setter methods for it.

--- src/main/java/com/gavinwernercodes/learningportal/entity/Course.java
+++ src/main/java/com/gavinwernercodes/learningportal/entity/Course.java
@@ -13,6 +13,9 @@ public class Course {
     @Column(name = "course_id")
     private Long course_id;
 
+    @Column(name = "course_title")
+    private String course_title;
+
 
     @ManyToOne
     @JoinColumn(name = "instructor_id")
@@ -25,12 +28,22 @@ public class Course {
     private List<Lesson> lessons;
 
 
-    // public getters and setters
+    // public getter and setter for course_id
     public Long getCourse_id() {
         return course_id;
     }
 
     public void setCourse_id(Long course_id) {
         this.course_id = course_id;
+    }
+
+    // getter and setter for course_title
+
+    public String getCourse_title() {
+        return course_title;
+    }
+
+    public void setCourse_title(String course_title) {
+        this.course_title = course_title;
     }
 }
